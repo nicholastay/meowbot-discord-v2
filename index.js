94a811c410c9cb532e4dcf303b667a6176dea31d
Nicholas Tay <nexerq@gmail.com>
2016-04-26 21:38:20 +1000

    Small performance boost with polyfilling in babelrc instead

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 /*eslint-disable semi */ // Allow semis as this is the es5 file
 
 require('babel-register');
-require('babel-polyfill'); // regen runtime
-
-require('./MeowBot');
+exports.MeowBot = require('./MeowBot');
